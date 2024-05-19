37f50f409a5c07c4e99c74a3fc4f78c5fadcb8af
unknown <mouatacim.med@gmail.com>
2024-05-19 17:36:40 +0200

    done

---
+++ client.c
@@ -0,0 +1,42 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+
+void	send_signal(int pid, unsigned char character)
+{
+	int				i;
+	unsigned char	temp_char;
+
+	i = 8;
+	temp_char = character;
+	while (i > 0)
+	{
+		i--;
+		temp_char = character >> i;
+		if (temp_char % 2 == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(42);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int		server_pid;
+	const char	*message;
+	int			i;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <pid> <message>\n", argv[0]);
+		exit(0);
+	}
+	server_pid = ft_atoi(argv[1]);
+	message = argv[2];
+	i = 0;
+	while (message[i])
+		send_signal(server_pid, message[i++]);
+	send_signal(server_pid, '\0');
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,34 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	handle_signal(int signal)
+{
+	static unsigned char	current_char;
+	static int				bit_index;
+
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == '\0')
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
+}
+
+int	main(void)
+{
+	printf("%d\n", getpid());
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
+	while (1)
+		pause();
+	return (0);
+}
